f6805515d441ad8841c8ff576dcd69a6873ce328
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:02:35 +0400

    DocumentRepository.java

--- src/main/java/com/example/TelecomE_ServicesSystem/repositories/DocumentRepository.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/repositories/DocumentRepository.java
@@ -1,4 +1,19 @@
 package com.example.TelecomE_ServicesSystem.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.util.List;
+import com.example.TelecomE_ServicesSystem.entites.*;
 
-public interface DocumentRepository {
+@Repository
+public interface DocumentRepository extends JpaRepository<Department, Long> {
+    @Query("SELECT x FROM Document x WHERE x.isActive=true")
+    List<Document> getAllDocument();
+
+    @Query("SELECT x FROM Document x WHERE x.isActive=true AND x.id=:id")
+    Document getById(@Param("id") Long id);
+
 }
