c34595a9279dbe8d364cc8f48302c8f1ef1856cf
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:02:55 +0400

    DomainRegistrationController.java

--- src/main/java/com/example/TelecomE_ServicesSystem/entites/DomainRegistration.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/entites/DomainRegistration.java
@@ -1,4 +1,23 @@
 package com.example.TelecomE_ServicesSystem.entites;
 
-public class DomainRegistration {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "domain_registrations")
+public class DomainRegistration extends BaseClass {
+    private String domainName;
+    private LocalDate registeredDate;
+    private LocalDate expiryDate;
+    private String status;
+
+    @ManyToOne
+    private Citizen citizen;
 }
