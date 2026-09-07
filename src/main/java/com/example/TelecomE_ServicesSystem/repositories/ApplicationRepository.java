23ec251d81513248b6ac287904ed0ef36118dd62
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:00:05 +0400

    ApplicationRepository.java

--- src/main/java/com/example/TelecomE_ServicesSystem/repositories/ApplicationRepository.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/repositories/ApplicationRepository.java
@@ -1,4 +1,23 @@
 package com.example.TelecomE_ServicesSystem.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
-public interface ApplicationRepository {
+import com.example.TelecomE_ServicesSystem.entites.*;
+@Repository
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
+    @Query("SELECT x FROM Application x WHERE x.isActive=true")
+    List<Application> getAllApplication();
+
+    @Query("SELECT x FROM Application x WHERE x.isActive=true AND x.id=:id")
+    Application getById(@Param("id") Long id);
+
+    @Query("SELECT a FROM Application a WHERE a.isActive=true AND LOWER(a.status)=LOWER(:status)")
+    List<Application> findByStatus(@Param("status") String status);
+
+    @Query("SELECT a FROM Application a WHERE a.isActive=true AND a.citizen.id=:citizenId ORDER BY a.applicationDate DESC")
+    List<Application> findByCitizen(@Param("citizenId") Long citizenId);
+
 }
