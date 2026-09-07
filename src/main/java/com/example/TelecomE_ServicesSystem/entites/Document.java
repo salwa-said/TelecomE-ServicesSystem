b1d23701dd3f3ae2a055316fa1e66825531e49d4
salwa-said <salwwa2000@gmail.com>
2026-09-07 15:56:43 +0400

    Document.java

--- src/main/java/com/example/TelecomE_ServicesSystem/entites/Document.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/entites/Document.java
@@ -1,4 +1,23 @@
 package com.example.TelecomE_ServicesSystem.entites;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Document {
+import java.time.LocalDate;
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "documents")
+public class Document extends BaseClass {
+    private String title;
+    private String type;
+    private LocalDate uploadDate;
+
+    @ManyToOne
+    private Application application;
+    @ManyToOne
+    private Project project;
 }
