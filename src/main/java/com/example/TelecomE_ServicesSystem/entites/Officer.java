f45b811df74680db30a5392002b001d21690d6ba
salwa-said <salwwa2000@gmail.com>
2026-09-07 15:57:28 +0400

    Officer.java

--- src/main/java/com/example/TelecomE_ServicesSystem/entites/Officer.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/entites/Officer.java
@@ -1,4 +1,30 @@
 package com.example.TelecomE_ServicesSystem.entites;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Officer {
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "officers")
+public class Officer extends BaseClass {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String designation;
+
+    @ManyToOne
+    private Department department;
+    @JsonIgnore
+    @OneToMany(mappedBy = "officer", cascade = CascadeType.ALL)
+    private List<Application> applications = new ArrayList<>();
+    @JsonIgnore
+    @OneToMany(mappedBy = "officer", cascade = CascadeType.ALL)
+    private List<Inspection> inspections = new ArrayList<>();
+    @JsonIgnore
+    @OneToMany(mappedBy = "officer", cascade = CascadeType.ALL)
+    private List<Complaint> complaints = new ArrayList<>();
 }
