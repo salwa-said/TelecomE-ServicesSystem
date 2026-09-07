9c47c3f3e785096c7648f36247c991206a817c46
salwa-said <salwwa2000@gmail.com>
2026-09-07 15:55:52 +0400

    Application.java

--- src/main/java/com/example/TelecomE_ServicesSystem/entites/Application.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/entites/Application.java
@@ -1,4 +1,31 @@
 package com.example.TelecomE_ServicesSystem.entites;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Application {
+import java.time.LocalDate;
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "applications")
+
+public class Application extends BaseClass {
+    private LocalDate applicationDate;
+    private String status;
+    private String referenceNumber;
+
+    @ManyToOne
+    private Citizen citizen;
+    @ManyToOne
+    private Service service;
+    @ManyToOne
+    private Officer officer;
+    @OneToOne(mappedBy = "application", cascade = CascadeType.ALL)
+    private Payment payment;
+
+    @OneToMany(mappedBy = "application", cascade = CascadeType.ALL)
+    private List<Document> documents = new ArrayList<>();
 }
