2755874f4e1cc4f0df7c3af436a08d2ea95b5ed9
salwa-said <salwwa2000@gmail.com>
2026-09-07 14:28:31 +0400

    Unversioned file

---
+++ src/main/java/com/example/TelecomE_ServicesSystem/TelecomEServicesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.TelecomE_ServicesSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomEServicesSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelecomEServicesSystemApplication.class, args);
+    }
+
+}
