fa217cec820d9184c5f6360aa207b7d0e3dd9164
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:52:51 +0400

    ResourceNotFoundException.java

--- src/main/java/com/example/TelecomE_ServicesSystem/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,5 @@
 package com.example.TelecomE_ServicesSystem.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
+    public ResourceNotFoundException(String message) { super(message); }
 }
