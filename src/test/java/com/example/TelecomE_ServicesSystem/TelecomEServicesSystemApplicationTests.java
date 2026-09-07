679eb6fc01b89f32f8f024581b90abca20abda69
salwa-said <salwwa2000@gmail.com>
2026-09-07 14:28:34 +0400

    Unversioned file

---
+++ src/test/java/com/example/TelecomE_ServicesSystem/TelecomEServicesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TelecomE_ServicesSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomEServicesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
