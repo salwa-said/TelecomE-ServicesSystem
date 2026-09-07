96c968c73042265639ceedb43c4303fd154c7f35
salwa-said <salwwa2000@gmail.com>
2026-09-07 15:57:54 +0400

    Project.java

--- src/main/java/com/example/TelecomE_ServicesSystem/entites/Project.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/entites/Project.java
@@ -1,4 +1,32 @@
 package com.example.TelecomE_ServicesSystem.entites;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Project {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "projects")
+public class Project extends BaseClass {
+    private String title;
+    private BigDecimal budget;
+    private LocalDate startDate;
+    private String status;
+
+    @ManyToOne
+    private Ministry ministry;
+    @JsonIgnore
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    private List<Milestone> milestones = new ArrayList<>();
+    @ManyToOne
+    private Vendor vendor;
+    @JsonIgnore
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    private List<Document> documents = new ArrayList<>();
 }
+
