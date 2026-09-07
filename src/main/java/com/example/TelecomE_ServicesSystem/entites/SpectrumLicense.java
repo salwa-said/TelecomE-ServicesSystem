31dcf316842a802232786343f9fedb6e46cfe43c
salwa-said <salwwa2000@gmail.com>
2026-09-07 15:58:14 +0400

    SpectrumLicense.java

--- src/main/java/com/example/TelecomE_ServicesSystem/entites/SpectrumLicense.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/entites/SpectrumLicense.java
@@ -1,4 +1,23 @@
 package com.example.TelecomE_ServicesSystem.entites;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-public class SpectrumLicense {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "spectrum_licenses")
+public class SpectrumLicense extends BaseClass {
+    private String bandName;
+    private BigDecimal frequencyMhz;
+    private LocalDate issueDate;
+    private LocalDate expiryDate;
+    private String status;
+
+    @ManyToOne
+    private Operator operator;
 }
