2c448ab17ddf2b64d9ddc2be1a6fc10cc918cbde
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:48:01 +0400

    BusinessException.java

---
+++ src/main/java/com/example/TelecomE_ServicesSystem/exceptions/BusinessException.java
@@ -0,0 +1,7 @@
+package com.example.TelecomE_ServicesSystem.exceptions;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
