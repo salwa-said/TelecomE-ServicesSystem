073e642f56bccb16514f8b77a42ed9a81b5cdcb1
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:18:17 +0400

    OfficerRepository.java

--- src/main/java/com/example/TelecomE_ServicesSystem/repositories/OfficerRepository.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/repositories/OfficerRepository.java
@@ -1,4 +1,20 @@
 package com.example.TelecomE_ServicesSystem.repositories;
+import com.example.TelecomE_ServicesSystem.entites.Officer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.util.List;
 
-public interface OfficerRepository {
+
+@Repository
+public interface OfficerRepository  extends JpaRepository<Officer, Long> {
+    @Query("SELECT x FROM Officer x WHERE x.isActive=true")
+    List<Officer> getAllOfficer();
+
+    @Query("SELECT x FROM Officer x WHERE x.isActive=true AND x.id=:id")
+    Officer getById(@Param("id") Long id);
+
 }
