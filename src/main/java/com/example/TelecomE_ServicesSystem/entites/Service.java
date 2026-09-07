de72a98e01f7aa17c1bcf58da04b55b5b4bca21f
salwa-said <salwwa2000@gmail.com>
2026-09-07 15:58:05 +0400

    Service.java

--- src/main/java/com/example/TelecomE_ServicesSystem/entites/Service.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/entites/Service.java
@@ -1,4 +1,25 @@
 package com.example.TelecomE_ServicesSystem.entites;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Service {
+import java.math.BigDecimal;
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "services")
+public class Service extends BaseClass {
+    private String name;
+    private String description;
+    private BigDecimal fee;
+    private Integer processingDays;
+
+    @ManyToOne
+    private Department department;
+    @JsonIgnore
+    @OneToMany(mappedBy = "service", cascade = CascadeType.ALL)
+    private List<Application> applications = new ArrayList<>();
 }
