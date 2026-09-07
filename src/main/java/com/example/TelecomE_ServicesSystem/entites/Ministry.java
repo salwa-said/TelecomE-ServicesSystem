5f4a0d2ddb7db331232324bade3f949b74d54855
salwa-said <salwwa2000@gmail.com>
2026-09-07 15:57:18 +0400

    Ministry.java

--- src/main/java/com/example/TelecomE_ServicesSystem/entites/Ministry.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/entites/Ministry.java
@@ -1,4 +1,25 @@
 package com.example.TelecomE_ServicesSystem.entites;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Ministry {
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "ministries")
+
+public class Ministry extends BaseClass {
+    private String name;
+    private String address;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "ministry", cascade = CascadeType.ALL)
+    private List<Department> departments = new ArrayList<>();
+    @JsonIgnore
+    @OneToMany(mappedBy = "ministry", cascade = CascadeType.ALL)
+    private List<Project> projects = new ArrayList<>();
 }
+
