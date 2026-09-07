206fe3411df3a31263c525e25eb816635732158c
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:18:49 +0400

    ServiceRepository.java

--- src/main/java/com/example/TelecomE_ServicesSystem/repositories/ServiceRepository.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/repositories/ServiceRepository.java
@@ -1,4 +1,21 @@
 package com.example.TelecomE_ServicesSystem.repositories;
 
-public interface ServiceRepository {
+import com.example.TelecomE_ServicesSystem.entites.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Repository
+public interface ServiceRepository  extends JpaRepository<Service, Long> {
+    @Query("SELECT x FROM Service x WHERE x.isActive=true")
+    List<Service> getAllService();
+
+    @Query("SELECT x FROM Service x WHERE x.isActive=true AND x.id=:id")
+    Service getById(@Param("id") Long id);
+
 }
+
