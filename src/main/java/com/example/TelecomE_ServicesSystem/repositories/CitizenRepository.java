d911edd622ed44b2c79eae393f072744b83b40b0
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:00:51 +0400

    CitizenRepository.java

--- src/main/java/com/example/TelecomE_ServicesSystem/repositories/CitizenRepository.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/repositories/CitizenRepository.java
@@ -1,4 +1,21 @@
 package com.example.TelecomE_ServicesSystem.repositories;
 
-public interface CitizenRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.util.List;
+import com.example.TelecomE_ServicesSystem.entites.*;
+
+@Repository
+
+public interface CitizenRepository extends JpaRepository<Citizen, Long> {
+    @Query("SELECT x FROM Citizen x WHERE x.isActive=true")
+    List<Citizen> getAllCitizen();
+
+    @Query("SELECT x FROM Citizen x WHERE x.isActive=true AND x.id=:id")
+    Citizen getById(@Param("id") Long id);
+
 }
