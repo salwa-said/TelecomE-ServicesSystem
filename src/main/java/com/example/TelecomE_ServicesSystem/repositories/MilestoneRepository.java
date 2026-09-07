4df68f669ebc5a088c58cf2df1343f6436ca3865
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:17:59 +0400

    MilestoneRepository.java

--- src/main/java/com/example/TelecomE_ServicesSystem/repositories/MilestoneRepository.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/repositories/MilestoneRepository.java
@@ -1,4 +1,19 @@
 package com.example.TelecomE_ServicesSystem.repositories;
+import com.example.TelecomE_ServicesSystem.entites.Milestone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.util.List;
 
-public interface MilestoneRepository {
+@Repository
+public interface MilestoneRepository extends JpaRepository<Milestone, Long> {
+    @Query("SELECT x FROM Milestone x WHERE x.isActive=true")
+    List<Milestone> getAllMilestone();
+
+    @Query("SELECT x FROM Milestone x WHERE x.isActive=true AND x.id=:id")
+    Milestone getById(@Param("id") Long id);
+
 }
