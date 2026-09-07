e025f6ceab63b9876e0af0c6454c663fe3164037
salwa-said <salwwa2000@gmail.com>
2026-09-07 15:56:54 +0400

    Inspection.java

--- src/main/java/com/example/TelecomE_ServicesSystem/entites/Inspection.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/entites/Inspection.java
@@ -1,4 +1,22 @@
 package com.example.TelecomE_ServicesSystem.entites;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Inspection {
+import java.time.LocalDate;
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "inspections")
+public class Inspection extends BaseClass {
+    private LocalDate inspectionDate;
+    private String result;
+    private String notes;
+
+    @ManyToOne
+    private Operator operator;
+    @ManyToOne
+    private Officer officer;
 }
