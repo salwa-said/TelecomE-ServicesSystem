666ff63dcab8af3b95219d2e9048ba250aa1350f
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:18:34 +0400

    PaymentRepository.java

--- src/main/java/com/example/TelecomE_ServicesSystem/repositories/PaymentRepository.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/repositories/PaymentRepository.java
@@ -1,4 +1,19 @@
 package com.example.TelecomE_ServicesSystem.repositories;
+import com.example.TelecomE_ServicesSystem.entites.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.util.List;
 
-public interface PaymentRepository {
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    @Query("SELECT x FROM Payment x WHERE x.isActive=true")
+    List<Payment> getAllPayment();
+
+    @Query("SELECT x FROM Payment x WHERE x.isActive=true AND x.id=:id")
+    Payment getById(@Param("id") Long id);
+
 }
