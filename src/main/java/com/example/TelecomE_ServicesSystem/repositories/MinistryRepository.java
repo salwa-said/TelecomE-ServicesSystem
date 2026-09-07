72c15d9785157c538b77a05186b11cfb5be96ca3
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:18:08 +0400

    MinistryRepository.java

--- src/main/java/com/example/TelecomE_ServicesSystem/repositories/MinistryRepository.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/repositories/MinistryRepository.java
@@ -1,4 +1,20 @@
 package com.example.TelecomE_ServicesSystem.repositories;
 
-public interface MinistryRepository {
+import com.example.TelecomE_ServicesSystem.entites.Ministry;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Repository
+public interface MinistryRepository extends JpaRepository<Ministry, Long> {
+    @Query("SELECT x FROM Ministry x WHERE x.isActive=true")
+    List<Ministry> getAllMinistry();
+
+    @Query("SELECT x FROM Ministry x WHERE x.isActive=true AND x.id=:id")
+    Ministry getById(@Param("id") Long id);
+
 }
