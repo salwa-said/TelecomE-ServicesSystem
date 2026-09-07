47437a51b54f5c9faaf309321ba1c82e4776fcf3
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:17:52 +0400

    InspectionRepository.java

--- src/main/java/com/example/TelecomE_ServicesSystem/repositories/InspectionRepository.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/repositories/InspectionRepository.java
@@ -1,4 +1,19 @@
 package com.example.TelecomE_ServicesSystem.repositories;
+import com.example.TelecomE_ServicesSystem.entites.Inspection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.util.List;
 
-public interface InspectionRepository {
+@Repository
+public interface InspectionRepository extends JpaRepository<Inspection, Long> {
+    @Query("SELECT x FROM Inspection x WHERE x.isActive=true")
+    List<Inspection> getAllInspection();
+
+    @Query("SELECT x FROM Inspection x WHERE x.isActive=true AND x.id=:id")
+    Inspection getById(@Param("id") Long id);
+
 }
