596720b8623a89b0c2ee4743f98fac91a0eaff47
salwa-said <salwwa2000@gmail.com>
2026-09-07 14:50:35 +0400

    ResourceNotFoundException

---
+++ src/main/java/com/example/TelecomE_ServicesSystem/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.TelecomE_ServicesSystem.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
