455007b1af1751044024c67e1cc80ba268e8fdc2
salwa-said <salwwa2000@gmail.com>
2026-09-07 15:58:40 +0400

    Payment.java

--- src/main/java/com/example/TelecomE_ServicesSystem/entites/Payment.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/entites/Payment.java
@@ -1,4 +1,24 @@
 package com.example.TelecomE_ServicesSystem.entites;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Payment {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "payments")
+public class Payment extends BaseClass {
+    private BigDecimal amount;
+    private String method;
+    private String status;
+    private LocalDate paidDate;
+
+    @OneToOne
+    @JoinColumn(name = "application_id", unique = true)
+    private Application application;
 }
+
