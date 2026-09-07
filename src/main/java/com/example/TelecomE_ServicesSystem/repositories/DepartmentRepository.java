84c9e757b36d6a0f140731bae511035036794964
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:02:12 +0400

    DepartmentRepository.java

--- src/main/java/com/example/TelecomE_ServicesSystem/repositories/DepartmentRepository.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/repositories/DepartmentRepository.java
@@ -1,4 +1,19 @@
 package com.example.TelecomE_ServicesSystem.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.util.List;
+import com.example.TelecomE_ServicesSystem.entites.*;
 
-public interface DepartmentRepository {
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    @Query("SELECT x FROM Department x WHERE x.isActive=true")
+    List<Department> getAllDepartment();
+
+    @Query("SELECT x FROM Department x WHERE x.isActive=true AND x.id=:id")
+    Department getById(@Param("id") Long id);
+
 }
