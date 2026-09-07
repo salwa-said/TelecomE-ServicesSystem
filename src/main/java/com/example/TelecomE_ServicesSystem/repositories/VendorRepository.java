93164a21b0e04bf54529d06d4b4be4188c74e58a
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:19:02 +0400

    VendorRepository.java

--- src/main/java/com/example/TelecomE_ServicesSystem/repositories/VendorRepository.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/repositories/VendorRepository.java
@@ -1,4 +1,18 @@
 package com.example.TelecomE_ServicesSystem.repositories;
+import com.example.TelecomE_ServicesSystem.entites.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.util.List;
+@Repository
+public interface VendorRepository extends JpaRepository<Vendor, Long> {
+    @Query("SELECT x FROM Vendor x WHERE x.isActive=true")
+    List<Vendor> getAllVendor();
 
-public interface VendorRepository {
+    @Query("SELECT x FROM Vendor x WHERE x.isActive=true AND x.id=:id")
+    Vendor getById(@Param("id") Long id);
+
 }
