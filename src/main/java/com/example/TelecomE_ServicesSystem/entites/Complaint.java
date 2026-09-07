9aaa71c2cfd6abd9143a91055aa0e008853faa72
salwa-said <salwwa2000@gmail.com>
2026-09-07 15:56:20 +0400

    Complaint.java

--- src/main/java/com/example/TelecomE_ServicesSystem/entites/Complaint.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/entites/Complaint.java
@@ -1,4 +1,25 @@
 package com.example.TelecomE_ServicesSystem.entites;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Complaint {
+import java.time.LocalDate;
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "complaints")
+public class Complaint extends BaseClass {
+    private String subject;
+    private String description;
+    private String status;
+    private LocalDate filedDate;
+
+    @ManyToOne
+    private Citizen citizen;
+    @ManyToOne
+    private Operator operator;
+    @ManyToOne
+    private Officer officer;
 }
