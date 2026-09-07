54d0211ce20b460f6ccb118c6ab0524ec9d80f75
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:18:27 +0400

    OperatorRepository.java

--- src/main/java/com/example/TelecomE_ServicesSystem/repositories/OperatorRepository.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/repositories/OperatorRepository.java
@@ -1,4 +1,20 @@
 package com.example.TelecomE_ServicesSystem.repositories;
+import com.example.TelecomE_ServicesSystem.entites.Operator;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.util.List;
 
-public interface OperatorRepository {
+
+@Repository
+public interface OperatorRepository extends JpaRepository<Operator, Long> {
+    @Query("SELECT x FROM Operator x WHERE x.isActive=true")
+    List<Operator> getAllOperator();
+
+    @Query("SELECT x FROM Operator x WHERE x.isActive=true AND x.id=:id")
+    Operator getById(@Param("id") Long id);
+
 }
