003047fc73c5cd64a7e0631b6f9b18735e3c8d37
salwa-said <salwwa2000@gmail.com>
2026-09-07 15:57:01 +0400

    Milestone.java

--- src/main/java/com/example/TelecomE_ServicesSystem/entites/Milestone.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/entites/Milestone.java
@@ -1,4 +1,20 @@
 package com.example.TelecomE_ServicesSystem.entites;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Milestone {
+import java.time.LocalDate;
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "milestones")
+public class Milestone  extends BaseClass {
+    private String title;
+    private LocalDate dueDate;
+    private String status;
+
+    @ManyToOne
+    private Project project;
 }
