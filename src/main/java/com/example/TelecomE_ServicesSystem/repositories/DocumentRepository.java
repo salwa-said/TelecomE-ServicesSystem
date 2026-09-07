ce16dc76095116ef76d021f1541671600243450d
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:17:34 +0400

    DocumentRepository.java

--- src/main/java/com/example/TelecomE_ServicesSystem/repositories/DocumentRepository.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/repositories/DocumentRepository.java
@@ -3,8 +3,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import java.time.LocalDate;
-import java.math.BigDecimal;
 import java.util.List;
 import com.example.TelecomE_ServicesSystem.entites.*;
 
