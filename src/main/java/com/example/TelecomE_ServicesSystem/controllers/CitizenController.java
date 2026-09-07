bf431d594220d6438588a9bb7a0b10ceed6888b7
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:48:16 +0400

    CitizenController.java

--- src/main/java/com/example/TelecomE_ServicesSystem/controllers/CitizenController.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/controllers/CitizenController.java
@@ -1,4 +1,47 @@
 package com.example.TelecomE_ServicesSystem.controllers;
 
+import com.example.TelecomE_ServicesSystem.dto.CitizenDTO;
+import com.example.TelecomE_ServicesSystem.entites.Citizen;
+import com.example.TelecomE_ServicesSystem.services.CitizenService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("citizen")
 public class CitizenController {
+    private CitizenService service;
+
+    @Autowired
+    public CitizenController(CitizenService service) {
+        this.service = service;
+    }
+
+    @PostMapping("add")
+    public Long add(@Valid @RequestBody CitizenDTO dto) {
+        return service.create(dto);
+    }
+
+    @GetMapping("getAll")
+    public List<CitizenDTO> getAll() {
+        return CitizenDTO.convertToDTO(service.getAll());
+    }
+
+    @GetMapping("getById")
+    public CitizenDTO getById(@RequestParam Long id) {
+        return CitizenDTO.convertToDTO(service.getById(id));
+    }
+
+    @PutMapping("update")
+    public CitizenDTO update(@Valid @RequestBody CitizenDTO dto) {
+        return CitizenDTO.convertToDTO(service.update(dto));
+    }
+
+    @DeleteMapping("deleteById")
+    public Boolean deleteById(@RequestParam Long id) {
+        return service.deleteById(id);
+    }
 }
+
