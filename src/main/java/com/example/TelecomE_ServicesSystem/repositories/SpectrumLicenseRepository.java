423de9ae8f8b2ac479da7b6656b316e2aebd6ad2
salwa-said <salwwa2000@gmail.com>
2026-09-07 16:18:56 +0400

    SpectrumLicenseRepository.java

--- src/main/java/com/example/TelecomE_ServicesSystem/repositories/SpectrumLicenseRepository.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/repositories/SpectrumLicenseRepository.java
@@ -1,4 +1,22 @@
 package com.example.TelecomE_ServicesSystem.repositories;
+import com.example.TelecomE_ServicesSystem.entites.SpectrumLicense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.util.List;
 
-public interface SpectrumLicenseRepository {
+@Repository
+public interface SpectrumLicenseRepository extends JpaRepository<SpectrumLicense, Long> {
+    @Query("SELECT x FROM SpectrumLicense x WHERE x.isActive=true")
+    List<SpectrumLicense> getAllSpectrumLicense();
+
+    @Query("SELECT x FROM SpectrumLicense x WHERE x.isActive=true AND x.id=:id")
+    SpectrumLicense getById(@Param("id") Long id);
+
+    @Query("SELECT s FROM SpectrumLicense s WHERE s.isActive=true AND s.expiryDate BETWEEN :today AND :soon")
+    List<SpectrumLicense> findExpiringSoon(@Param("today") LocalDate today, @Param("soon") LocalDate soon);
+
 }
