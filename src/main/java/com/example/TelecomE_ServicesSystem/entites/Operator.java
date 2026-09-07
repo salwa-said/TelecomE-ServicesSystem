7200d5766b01d389d44a728c4adc7ddebdd20754
salwa-said <salwwa2000@gmail.com>
2026-09-07 15:57:42 +0400

    Operator.java

--- src/main/java/com/example/TelecomE_ServicesSystem/entites/Operator.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/entites/Operator.java
@@ -1,4 +1,29 @@
 package com.example.TelecomE_ServicesSystem.entites;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Operator {
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "operators")
+public class Operator extends BaseClass {
+    private String name;
+    private String licenseNumber;
+    private String contactEmail;
+    private String country;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "operator", cascade = CascadeType.ALL)
+    private List<SpectrumLicense> spectrumLicenses = new ArrayList<>();
+    @JsonIgnore
+    @OneToMany(mappedBy = "operator", cascade = CascadeType.ALL)
+    private List<Inspection> inspections = new ArrayList<>();
+    @JsonIgnore
+    @OneToMany(mappedBy = "operator", cascade = CascadeType.ALL)
+    private List<Complaint> complaints = new ArrayList<>();
 }
+
