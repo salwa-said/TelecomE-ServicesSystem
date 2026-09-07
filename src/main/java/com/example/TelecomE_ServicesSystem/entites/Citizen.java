6ced62f8edecad698ea1a9f97d01b4df88d13250
salwa-said <salwwa2000@gmail.com>
2026-09-07 15:56:05 +0400

    Citizen.java

--- src/main/java/com/example/TelecomE_ServicesSystem/entites/Citizen.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/entites/Citizen.java
@@ -1,4 +1,28 @@
 package com.example.TelecomE_ServicesSystem.entites;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Citizen {
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "citizens")
+public class Citizen extends BaseClass {
+    private String name;
+    private String nationalId;
+    private String phoneNumber;
+    private String email;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "citizen", cascade = CascadeType.ALL)
+    private List<Application> applications = new ArrayList<>();
+    @JsonIgnore
+    @OneToMany(mappedBy = "citizen", cascade = CascadeType.ALL)
+    private List<Complaint> complaints = new ArrayList<>();
+    @JsonIgnore
+    @OneToMany(mappedBy = "citizen", cascade = CascadeType.ALL)
+    private List<DomainRegistration> domainRegistrations = new ArrayList<>();
 }
