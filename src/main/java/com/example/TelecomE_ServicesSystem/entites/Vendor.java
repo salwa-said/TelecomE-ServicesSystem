68ecd1d5a6d1166499420f68179be502f9350f3c
salwa-said <salwwa2000@gmail.com>
2026-09-07 15:58:30 +0400

    Vendor.java

--- src/main/java/com/example/TelecomE_ServicesSystem/entites/Vendor.java
+++ src/main/java/com/example/TelecomE_ServicesSystem/entites/Vendor.java
@@ -1,4 +1,23 @@
 package com.example.TelecomE_ServicesSystem.entites;
 
-public class Vendor {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "vendors")
+public class Vendor extends BaseClass {
+    private String name;
+    private String contactEmail;
+    private String phoneNumber;
+    private String country;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "vendor", cascade = CascadeType.ALL)
+    private List<Project> projects = new ArrayList<>();
 }
